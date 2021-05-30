82f6a91f7399067f08447c36e661d303617fcbd7
Zdeněk Kořán <zkoran@gmail.com>
2021-05-30 17:44:06 +0200

    Fix color of TextFields
    Text fields in pop-ups had wrong accent color (blue) when dark mode was active.

--- lib/main.dart
+++ lib/main.dart
@@ -20,15 +20,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
+    final lightTheme = ThemeData(
+        primarySwatch: Colors.teal,
+        appBarTheme: new AppBarTheme(brightness: Brightness.dark),
+    );
+
+    // Dark theme ignores primarySwatch color (https://github.com/flutter/flutter/issues/19089)
+    // luckily it uses teal, so no problem here, since app primary color is also teal
+    final darkTheme = ThemeData(primarySwatch: Colors.teal, brightness: Brightness.dark);
+
     return MaterialApp(
       title: 'Counter',
-      theme: ThemeData(
-        primarySwatch: Colors.teal,
-        appBarTheme: new AppBarTheme(brightness: Brightness.dark)
-      ),
-      // Dark theme ignores primarySwatch color (https://github.com/flutter/flutter/issues/19089)
-      // luckily it uses teal, so no problem here, since app primary color is also teal
-      darkTheme: ThemeData.dark(),
+      theme: lightTheme,
+      darkTheme: darkTheme,
       themeMode: ThemeMode.system,
       home: MyHomePage(title: 'Counter'), // becomes the route named '/'
       routes: {
